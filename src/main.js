b88caae1b59d6e550cf5d1018e213fb14241b419
Valen Michael <michael.valen@telenor.no>
2023-04-18 00:41:44 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createRouter} from 'vue-router'
 import App from './App.vue'
+import CloseBuy from "../src/components/CloseBuy.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/closebuy',
+        name: 'Home',
+        component: CloseBuy,
+
+    },
+]
+
+const router = createRouter({
+    //history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+createApp(App).use(router).mount('#app')
